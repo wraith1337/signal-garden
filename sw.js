1dd1aeab4db5b19eab8bd477a91c5fde22705755
Wraith1337 <3m0ura@mail.instinct.com>
2026-09-16 14:44:36 -0400

    Add offline support

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='signal-garden-v2';
+const CORE=['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}))) });
